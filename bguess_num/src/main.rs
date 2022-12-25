eb4ec105cbc96a3382bdfb31c4baa40d322e7bda
jacky2code <jacky2code@gmail.com>
2022-12-25 19:27:19 +0800

    guess number

--- bguess_num/src/main.rs
+++ bguess_num/src/main.rs
@@ -2,17 +2,23 @@
  * @Author: GKing
  * @Date: 2022-12-17 10:46:24
  * @LastEditors: GKing
- * @LastEditTime: 2022-12-17 17:04:34
+ * @LastEditTime: 2022-12-25 11:21:23
  * @Description: 
  * 生成一个 1 到 100 的随机数
  * 提示玩家输入一个猜测数字
  * 猜完之后，程序会提示猜测的大小
  * @TODO: 
  */
-use std::io;
+use std::io;    // prelude
+use rand::Rng;  // trait
+use std::cmp::Ordering;
 
 fn main() {
     println!("猜数");
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("神秘数字是: {}", secret_number);
+
     println!("猜测一个数");
 
     // mut 可变变量
@@ -21,6 +27,16 @@ fn main() {
     // 通过io:stdin() 进行逐行读取
     io::stdin().read_line(&mut guess).expect("Can not read line!");
 
+    // string类型 转换 number 类型
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("Your guess number is: {}", guess);
+
+    // 比较大小
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!")       
+    }
 }
  
